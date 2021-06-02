734d5e5344c9df2f249b1b0b132c81ba5e6528f6
lolsiscool <85200522+lolsiscool@users.noreply.github.com>
2021-06-01 19:34:02 -0500

    Update Remove.js

--- Remove.js
+++ Remove.js
@@ -1,3 +1,5 @@
+Trigger = Discord: message.create
+
 // authenticates you with the API standard library
 // type `await lib.` to display API autocomplete
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
