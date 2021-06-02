bdf897f309d2b2839f7baac405dd57e4aa76721f
lolsiscool <85200522+lolsiscool@users.noreply.github.com>
2021-06-01 19:23:13 -0500

    Delete Exit.js

--- Exit.js
+++
@@ -1,8 +0,0 @@
-// authenticates you with the API standard library
-// type `await lib.` to display API autocomplete
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-
-await lib.discord.channels['@0.1.1'].messages.destroy({
-  message_id: `${context.params.event.message.id}`,
-  channel_id: `${context.params.event.message.channel_id}`
-});
